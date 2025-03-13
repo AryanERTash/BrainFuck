fcfc3a99b7bfb916fd58d3116d4915803c2c63c2
Aryan Kumar <rahuljaikarkumar7042@gmail.com>
2025-03-13 21:26:43 +0530

    main.c: Added file support for brainf*ck

---
+++ README.md
@@ -0,0 +1,8 @@
+
+
+## 📄 About Brainfuck
+
+Brainfuck is a minimalist, esoteric programming language that operates on a memory tape. It consists of only **eight commands**:  
+`>`, `<`, `+`, `-`, `.` `,`, `[`, and `]`.
+
+
--- main.c
+++ main.c
@@ -1,85 +1,117 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #define ARRAY_LENGTH 30000
 
-char ARRAY[ARRAY_LENGTH];	// Array to be initialized with 0
-int ARRAY_POINTER = 0;	// the global array pointer
-int NUM_BRACK = 0;	// bracket checking
-int INS_PTR = 0;	// Instruction pointer
+char ARRAY[ARRAY_LENGTH]; // Memory array
+int ARRAY_POINTER = 0;	  // Memory pointer
+int INS_PTR = 0;		  // Instruction pointer
 
-void evaluate_bf(char *expression, int brack_index)
+void evaluate_bf(char *expression);
+char *read_file(const char *filename);
+
+void evaluate_bf(char *expression)
 {
+	char current;
+	int loop;
 
-   char current;
-
-   while ((current = expression[INS_PTR]) != 0)
-   {
-      
-      if (current == '>')
-      {
-         ARRAY_POINTER++;
-         INS_PTR++;
-      }
-      else if (current == '<')
-      {
-         ARRAY_POINTER--;
-         INS_PTR++;
-      }
-      else if (current == '+')
-      {
-         ARRAY[ARRAY_POINTER]++;
-         INS_PTR++;
-      }
-      else if (current == '-')
-      {
-         ARRAY[ARRAY_POINTER]--;
-         INS_PTR++;
-      }
-      else if (current == '.')
-      {
-         putchar(ARRAY[ARRAY_POINTER]);
-         INS_PTR++;
-      }
-      else if (current == ',')
-      {
-         ARRAY[ARRAY_POINTER] = (char)getchar();
-         INS_PTR++;
-      }
-      else if (current == '[')
-      {
-         NUM_BRACK++;
-         INS_PTR++;
-         evaluate_bf(expression, INS_PTR);
-         
-      }
-      else if (current == ']')
-      {
-         if(ARRAY[ARRAY_POINTER]==0) {
-            INS_PTR++;
-            NUM_BRACK--;
-            return;
-         }
-         INS_PTR=brack_index;         
-      }
-
-      continue;
-
-      
-   }
-   if(NUM_BRACK!=0) {
-
-      fprintf(stderr, "Not correct brackets");
-      NUM_BRACK=0;
-   }
+	while ((current = expression[INS_PTR]) != 0)
+	{
+		switch (current)
+		{
+		case '>':
+			ARRAY_POINTER++;
+			break;
+		case '<':
+			ARRAY_POINTER--;
+			break;
+		case '+':
+			ARRAY[ARRAY_POINTER]++;
+			break;
+		case '-':
+			ARRAY[ARRAY_POINTER]--;
+			break;
+		case '.':
+			putchar(ARRAY[ARRAY_POINTER]);
+			fflush(stdout);
+			break;
+		case ',':
+			ARRAY[ARRAY_POINTER] = (char)getchar();
+			break;
+		case '[':
+			if (ARRAY[ARRAY_POINTER] == 0)
+			{
+				loop = 1;
+				while (loop > 0)
+				{
+					INS_PTR++;
+					if (expression[INS_PTR] == '[')
+						loop++;
+					else if (expression[INS_PTR] == ']')
+						loop--;
+				}
+			}
+			break;
+		case ']':
+			if (ARRAY[ARRAY_POINTER] != 0)
+			{
+				loop = 1;
+				while (loop > 0)
+				{
+					INS_PTR--;
+					if (expression[INS_PTR] == '[')
+						loop--;
+					else if (expression[INS_PTR] == ']')
+						loop++;
+				}
+				INS_PTR--;
+			}
+			break;
+		}
+		INS_PTR++;
+	}
 }
+
+char *read_file(const char *filename)
+{
+	FILE *file = fopen(filename, "r");
+	if (!file)
+	{
+		fprintf(stderr, "Error: Could not open file %s\n", filename);
+		exit(EXIT_FAILURE);
+	}
+
+	fseek(file, 0, SEEK_END);
+	long length = ftell(file);
+	rewind(file);
+
+	char *buffer = (char *)malloc(length + 1);
+
+	fread(buffer, 1, length, file); // read all at once
+
+	buffer[length] = '\0';
+	fclose(file);
+
+	return buffer;
+}
+
 int main(int argc, char const *argv[])
 {
-   for(int i=0;i<ARRAY_LENGTH;++i)
-      ARRAY[i]=0;
-   evaluate_bf("++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.", 0);
-   printf("\n");
-   
-   return 0;
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <brainfuck_file.bf>\n", argv[0]);
+		return EXIT_FAILURE;
+	}
+
+	memset(ARRAY, 0, ARRAY_LENGTH);
+
+	char *code = read_file(argv[1]);
+
+	evaluate_bf(code);
+	putchar('\n');
+	free(code);
+
+	return EXIT_SUCCESS;
 }
